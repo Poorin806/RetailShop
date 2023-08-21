d83ec4ef27a669e1a11d74313d4c1dc579bfcdc9
Automatic28m <phanlop.auto@gmail.com>
2023-08-21 08:39:52 +0700

    -djwaionsd

--- management/list_buy.php
+++ management/list_buy.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>รายการซื้อสินค้าเข้า</title>
     <!-- Essentials Css Icons -->
     <?php include_once "../import/css.php"; ?>
 
